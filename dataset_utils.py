a48aa0c539e757a36e47648cfb684c03b243ad3c
Anton Fedotov <fedotov.anton.af@gmail.com>
2019-05-14 20:42:56 +0300

    fix some errors

--- dataset_utils.py
+++ dataset_utils.py
@@ -1,3 +1,39 @@
+import base64
+import json
+import os
+import zlib
+from abc import ABCMeta, abstractmethod
+
+import cv2
+import numpy as np
+import torch
+from albumentations import OneOf, RandomCrop, Resize, Compose, HorizontalFlip, VerticalFlip, RandomRotate90, RandomBrightnessContrast, RandomGamma, RGBShift, SmallestMaxSize, GaussNoise, MedianBlur, JpegCompression
+from neural_pipeline import AbstractDataset, DataProducer
+
+base_dir = 'datasets/pixart'
+
+
+class PixartDataset(AbstractDataset):
+    def __init__(self, images_pathes: []):
+        images_dir = os.path.join(base_dir, 'train')
+        masks_dir = os.path.join(base_dir, 'train_mask')
+        images_pathes = sorted(images_pathes, key=lambda p: int(os.path.splitext(p)[0]))
+        self._image_pathes = []
+        for p in images_pathes:
+            name = os.path.splitext(p)[0]
+            mask_img = os.path.join(masks_dir, name + '.png')
+            if os.path.exists(mask_img):
+                path = {'data': os.path.join(images_dir, p), 'target': mask_img}
+                self._image_pathes.append(path)
+
+    def __len__(self):
+        return len(self._image_pathes)
+
+    def __getitem__(self, item):
+        img = cv2.cvtColor(cv2.imread(self._image_pathes[item]['data']), cv2.COLOR_BGR2RGB)
+        return {'data': img, 'target': cv2.imread(self._image_pathes[item]['target'], cv2.IMREAD_UNCHANGED) / 255}
+
+
 class MasksComposer:
     def __init__(self, target_shape: [], dtype: np.typename = np.uint8):
         self._masks = {}
@@ -57,12 +93,12 @@ class MasksComposer:
         for cls, mask in self._masks.items():
             if res is None:
                 if isinstance(mask, dict):
-                    res = np.stack((mask['mask'], mask['borders']), axis=0)
+                    res = np.stack((mask['mask'], mask['borders']), axis=2)
                 else:
                     res = mask
             else:
                 if isinstance(mask, dict):
-                    res = np.stack((res, mask['mask'], mask['borders']), axis=0)
+                    res = np.stack((res, mask['mask'], mask['borders']), axis=2)
                 else:
                     res = np.stack((res, mask), axis=0)
         return res
@@ -136,8 +172,10 @@ class SuperviselyDataset(AbstractDataset):
 
             elif len(obj['points']['interior']) + len(obj['points']['exterior']) > 0:
                 pts = np.array(obj['points']['exterior'], dtype=np.int)
-                obj_mask = cv2.drawContours(mask.copy(), [pts], -1, 1, cv2.FILLED)
                 origin = pts.min(axis=0)
+                shape = pts.max(axis=0) - origin
+                obj_mask = cv2.drawContours(np.zeros((shape[1], shape[0]), dtype=np.uint8), [pts - origin], -1, 1, cv2.FILLED)
+                origin = np.array([origin[1], origin[0]], dtype=np.int)
 
             return obj_mask, origin
 
@@ -176,33 +214,48 @@ class EmptyClassesAdd:
         cur_res = self._dataset[item]
         res = {'data': cur_res['data']}
         target_shape = cur_res['target'].shape
-        if target_shape[0] > self._target_classes_num:
-            raise RuntimeError("Dataset produce target with shape, that's first dimension greater than target classes num."
-                               "Target shape from dataset: {}, target classes num: {}".format(target_shape, self._target_classes_num))
         if len(target_shape) > 3:
             raise RuntimeError("Dataset produce target with channels number more than 3."
                                "Target shape from dataset: {}".format(target_shape))
-        target_shape = (self._target_classes_num, target_shape[0], target_shape[1])
-        target = np.zeros_like(target_shape)
-        target[self._exists_class_idx, :, :] = cur_res['shape']
+        elif len(target_shape) == 3 and target_shape[2] > self._target_classes_num:
+            raise RuntimeError("Dataset produce target with shape, that's first dimension greater than target classes num."
+                               "Target shape from dataset: {}, target classes num: {}".format(target_shape, self._target_classes_num))
+
+        target_shape = (target_shape[0], target_shape[1], self._target_classes_num)
+        target = np.zeros(target_shape, dtype=np.uint8)
+        target[:, :, self._exists_class_idx] = cur_res['target']
         res['target'] = target
         return res
+
+    def __len__(self):
+        return len(self._dataset)
 
 
 class AugmentedDataset:
     def __init__(self, dataset):
         self._dataset = dataset
-        self._augmentations = {}
+        self._augs = {}
+        self._augs_for_whole = []
 
-    def add_aug(self, aug: callable, identificator) -> 'AugmentedDataset':
-        self._augmentations[identificator] = aug
+    def add_aug(self, aug: callable, identificator=None) -> 'AugmentedDataset':
+        if identificator is None:
+            self._augs_for_whole.append(aug)
+        else:
+            self._augs[identificator] = aug
         return self
 
     def __getitem__(self, item):
         res = self._dataset[item]
         for k, v in res.items() if isinstance(res, dict) else enumerate(res):
-            res[k] = self._augmentations[k](v)
+            if k in self._augs:
+                res[k] = self._augs[k](v)
+        for aug in self._augs_for_whole:
+            res = aug(res)
         return res
+
+    def __len__(self):
+        return len(self._dataset)
+
 
 class SegmentationVisualizer(metaclass=ABCMeta):
     @abstractmethod
@@ -227,23 +280,53 @@ class ColormapVisualizer(SegmentationVisualizer):
 
 
 class MulticlassColormapVisualizer(ColormapVisualizer):
-    def __init__(self, main_class: int, proportions: [float, float], colormap=cv2.COLORMAP_JET, other_colors: []=None):
+    def __init__(self, main_class: int, proportions: [float, float], colormap=cv2.COLORMAP_JET, other_colors: [] = None):
         super().__init__(proportions, colormap)
 
         self._main_class = main_class
         self._other_colors = other_colors
 
     def process_img(self, image, mask) -> np.ndarray:
-        main_target = mask[self._main_class, :, :]
-        other_classes = np.delete(mask, self._main_class, 0)
+        main_target = mask[:, :, self._main_class]
+        other_classes = np.delete(mask, self._main_class, 2)
         img = super().process_img(image, main_target)
 
         if self._other_colors is None:
             self._other_colors = np.array([np.linspace(127, 0, num=other_classes.shape[0], dtype=np.uint8),
                                            np.linspace(255, 127, num=other_classes.shape[0], dtype=np.uint8),
                                            np.linspace(127, 255, num=other_classes.shape[0], dtype=np.uint8)], dtype=np.uint8)
-        for i, cls in enumerate(other_classes):
+        for i in range(other_classes.shape[2]):
+            cls = other_classes[:, :, i]
             img[:, :, 0][cls > 0] = self._other_colors[0][i]
             img[:, :, 1][cls > 0] = self._other_colors[1][i]
             img[:, :, 2][cls > 0] = self._other_colors[2][i]
         return img
+
+
+def to_pytorch(**item):
+    return {'image': torch.from_numpy(np.moveaxis(item['image'].astype(np.float32) / 128 - 1, -1, 0)),
+            'mask': torch.from_numpy(np.moveaxis(item['mask'].astype(np.float32), -1, 0))}
+
+
+def create_augmented_dataset(indices: str, with_to_pytorch: bool = False):
+    with open(os.path.join('data', indices), 'r') as file:
+        pathes = [p[:len(p) - 1] for p in file.readlines() if p != '']
+    dataset_pixart = EmptyClassesAdd(PixartDataset(pathes), 2, 0)
+
+    dataset_supervisely = SuperviselyDataset(r'/home/toodef/data/datasets/my/Supervisely Person Dataset', include_not_marked_people=False, include_neutral_objects=False)
+    dataset_supervisely.use_border_as_class(5)
+
+    preprocess = Compose([SmallestMaxSize(512, always_apply=True, p=1), OneOf([Compose([RandomCrop(height=448, width=448), Resize(224, 224)], p=1), Resize(width=224, height=224)], p=1)])
+
+    transforms = Compose([HorizontalFlip(), RandomRotate90()], p=0.5)
+    aug = Compose([preprocess, transforms, RandomBrightnessContrast(brightness_limit=0.4, contrast_limit=0.4), MedianBlur(blur_limit=4), JpegCompression(quality_lower=50),
+                   RandomGamma(), RGBShift(), GaussNoise(), transforms])
+
+    if with_to_pytorch:
+        aug = Compose([aug, to_pytorch])
+
+    def augmentate(data: {}):
+        res = aug(image=data['data'], mask=data['target'])
+        return {'data': res['image'], 'target': res['mask']}
+
+    return AugmentedDataset(dataset_pixart).add_aug(augmentate), AugmentedDataset(dataset_supervisely).add_aug(augmentate)
