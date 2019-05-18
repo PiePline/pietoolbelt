improve metrics tests; try to fix losses; correct calculation iou for boxes

cv_utils/losses/segmentation.py
@@ -2,7 +2,7 @@ import torch
 from torch import Tensor
 from torch.nn import Module
 
-from cv_utils.losses.common import ComposedLoss
+from cv_utils.losses.common import ComposedLoss, Reduction
 from cv_utils.metrics.torch.segmentation import dice, _multiclass_metric
 from cv_utils.models.utils import Activation
 
@@ -15,13 +15,14 @@ class DiceLoss(Module):
         eps (float): smooth value. When eps == 1 it's named Smooth Dice loss
         activation (srt): the activation function, that applied to predicted values. See :class:`Activation` for possible values
     """
-    def __init__(self, eps: float = 1, activation: str = None):
+    def __init__(self, eps: float = 1, activation: str = None, reduction: Reduction = None):
         super().__init__()
         self._activation = Activation(activation)
+        self._reduction = lambda x: x if reduction is None else reduction
         self._eps = eps
 
     def forward(self, output: Tensor, target: Tensor):
-        return 1 - dice(self._activation(output), target, eps=self._eps)
+        return self._reduction(1 - dice(self._activation(output), target, eps=self._eps))
 
 
 class BCEDiceLoss(ComposedLoss):
@@ -34,9 +35,9 @@ class BCEDiceLoss(ComposedLoss):
         eps (float): smooth value. When eps == 1 it's named Smooth Dice loss
         activation (srt): the activation function, that applied to predicted values. See :class:`Activation` for possible values
     """
-    def __init__(self, bce_w: float, dice_w: float, eps: float = 1, activation: str = None):
+    def __init__(self, bce_w: float, dice_w: float, eps: float = 1, activation: str = None, reduction: Reduction = None):
         bce_loss = torch.nn.BCEWithLogitsLoss()
-        dice_loss = DiceLoss(eps=eps, activation=activation)
+        dice_loss = DiceLoss(eps=eps, activation=activation, reduction=reduction)
 
         super().__init__([bce_loss, dice_loss], [bce_w, dice_w])
 
@@ -49,9 +50,10 @@ class MulticlassSegmentationLoss(Module):
     Args:
          base_loss (Module): basic loss object
     """
-    def __init__(self, base_loss: Module):
+    def __init__(self, base_loss: Module, reduction: Reduction = None):
         super().__init__()
         self._base_loss = base_loss
+        self._reduction = lambda x: x if reduction is None else reduction
 
     def forward(self, output, target):
-        return _multiclass_metric(self._base_loss, output, target)
+        return self._reduction(_multiclass_metric(self._base_loss, output, target))

cv_utils/metrics/cpu/detection.py
@@ -16,11 +16,11 @@ def _compute_boxes_iou(box: np.ndarray or [], boxes: np.ndarray or [], box_area:
     Note: the areas are passed in rather than calculated here for
           efficency. Calculate once in the caller to avoid duplicate work.
     """
-    y1 = np.maximum(box[0], boxes[:, :, 0])
-    x1 = np.maximum(box[1], boxes[:, :, 1])
-    x2 = np.minimum(box[2], boxes[:, :, 2])
-    y2 = np.minimum(box[3], boxes[:, :, 3])
-    intersection = np.maximum(x2 - x1, 1) * np.maximum(y2 - y1, 1)
+    xmin = np.maximum(box[0], boxes[:, :, 0])
+    ymin = np.maximum(box[1], boxes[:, :, 1])
+    xmax = np.minimum(box[2], boxes[:, :, 2])
+    ymax = np.minimum(box[3], boxes[:, :, 3])
+    intersection = (xmax - xmin) * (ymax - ymin)
     union = box_area + boxes_area[:] - intersection[:]
     iou = intersection / union
     return iou

cv_utils/metrics/torch/segmentation.py
@@ -103,13 +103,14 @@ def multiclass_jaccard(pred: Tensor, target: Tensor, eps: float = 1e-7) -> Tenso
 
 
 class _SegmentationMetric(AbstractMetric):
-    def __init__(self, name: str, func: callable, activation: str = None):
+    def __init__(self, name: str, func: callable, activation: str = None, eps: float = 1e-7):
         super().__init__(name)
-        self._activation = Activation(activation)
         self._func = func
+        self._activation = Activation(activation)
+        self._eps = eps
 
     def calc(self, output: Tensor, target: Tensor) -> np.ndarray:
-        return np.squeeze(self._func(self._activation(output), target).cpu().numpy())
+        return np.squeeze(self._func(self._activation(output), target, self._eps).cpu().numpy())
 
     @staticmethod
     def min_val() -> float:
@@ -121,11 +122,13 @@ class _SegmentationMetric(AbstractMetric):
 
 
 class MulticlassSegmentationMetric(_SegmentationMetric):
-    def __init__(self, name: str, func: callable, activation: str = None, reduction: str = 'sum'):
+    def __init__(self, name: str, func: callable, activation: str = None, reduction: str = None):
         super().__init__(name, func, activation)
 
-        if reduction == 'sum':
-            self._reduction = lambda x: x.sum(0) / x.shape(0)
+        if reduction is None:
+            self._reduction = lambda x: x
+        elif reduction == 'sum':
+            self._reduction = lambda x: x.sum(0)
         elif reduction == 'mean':
             self._reduction = lambda x: x.mean(0)
         else:
@@ -133,17 +136,17 @@ class MulticlassSegmentationMetric(_SegmentationMetric):
 
     def calc(self, output: Tensor, target: Tensor) -> np.ndarray:
         res = np.squeeze(self._func(self._activation(output), target).cpu().numpy())
-        return self._reduction(res.sum(0))
+        return self._reduction(res)
 
 
 class DiceMetric(_SegmentationMetric):
-    def __init__(self, activation: str = None):
-        super().__init__('dice', dice, activation)
+    def __init__(self, activation: str = None, eps: float = 1e-7):
+        super().__init__('dice', dice, activation, eps)
 
 
 class JaccardMetric(_SegmentationMetric):
-    def __init__(self, activation: str = None):
-        super().__init__('jaccard', jaccard, activation)
+    def __init__(self, activation: str = None, eps: float = 1e-7):
+        super().__init__('jaccard', jaccard, activation, eps)
 
 
 class SegmentationMetricsProcessor(MetricsProcessor):
@@ -166,4 +169,4 @@ class MulticlassSegmentationMetricsProcessor(MetricsProcessor):
     def __init__(self, stage_name: str, activation: str = None, reduction: str = 'sum'):
         super().__init__()
         self.add_metrics_group(MetricsGroup(stage_name).add(MulticlassJaccardMetric(activation=activation, reduction=reduction))
-                                                       .add(MulticlassDiceMetric(activation=activation, reduction=reduction)))
+                               .add(MulticlassDiceMetric(activation=activation, reduction=reduction)))

tests/metrics/torch/segmentation.py
@@ -1,7 +1,10 @@
 import unittest
 import torch
+from torch import Tensor
+import numpy as np
 
-from cv_utils.metrics.torch.segmentation import jaccard, dice, multiclass_jaccard, multiclass_dice
+from cv_utils.metrics.torch.segmentation import jaccard, dice, multiclass_jaccard, multiclass_dice, DiceMetric, MulticlassDiceMetric, \
+    JaccardMetric, MulticlassJaccardMetric
 from cv_utils.metrics.common import jaccard2dice, dice2jaccard
 
 
@@ -10,70 +13,10 @@ __all__ = ['PyTorchTest']
 
 class PyTorchTest(unittest.TestCase):
     def test_jaccard(self):
-        pred, target = torch.ones((1, 10, 10)), torch.ones((1, 10, 10))
-        val = jaccard(pred, target)
-        self.assertIsInstance(val, torch.Tensor)
-        self.assertTrue(torch.allclose(val, torch.tensor([1], dtype=torch.float32)))
-        self.assertEqual(pred.size(), torch.Size([1, 10, 10]))
-        self.assertEqual(target.size(), torch.Size([1, 10, 10]))
-
-        pred, target = torch.ones((2, 10, 10)), torch.ones((2, 10, 10))
-        pred[0, :, :] = 0
-        val = jaccard(pred, target)
-        self.assertIsInstance(val, torch.Tensor)
-        self.assertTrue(torch.allclose(val, torch.tensor([0, 1], dtype=torch.float32)))
-        self.assertEqual(pred.size(), torch.Size([2, 10, 10]))
-        self.assertEqual(target.size(), torch.Size([2, 10, 10]))
-
-        pred, target = torch.ones((2, 3, 10, 10)), torch.ones((2, 3, 10, 10))
-        pred[0, 0, :5, :] = 0
-        target[0, 0, :, :5] = 0
-        val = multiclass_jaccard(pred, target)
-        self.assertIsInstance(val, torch.Tensor)
-        self.assertTrue(torch.allclose(val, torch.tensor([[1 / 3, 1], [1, 1], [1, 1]], dtype=torch.float32)))
-        self.assertEqual(pred.size(), torch.Size([2, 3, 10, 10]))
-        self.assertEqual(target.size(), torch.Size([2, 3, 10, 10]))
-
-        pred, target = torch.zeros((1, 10, 10)), torch.zeros((1, 10, 10))
-        self.assertTrue(torch.allclose(jaccard(pred, target), torch.tensor([1], dtype=torch.float32)))
-
-        pred[0, :5, :] = 1
-        target[0, :, :5] = 1
-        val = jaccard(pred, target)
-        self.assertTrue(torch.allclose(val, torch.tensor([1 / 3], dtype=torch.float32)))
+        self._test_metric(jaccard, multiclass_jaccard, 1 / 3, self._test_functional_metric)
 
     def test_dice(self):
-        pred, target = torch.ones((1, 10, 10)), torch.ones((1, 10, 10))
-        val = dice(pred, target)
-        self.assertIsInstance(val, torch.Tensor)
-        self.assertTrue(torch.allclose(val, torch.tensor([1], dtype=torch.float32)))
-        self.assertEqual(pred.size(), torch.Size([1, 10, 10]))
-        self.assertEqual(target.size(), torch.Size([1, 10, 10]))
-
-        pred, target = torch.ones((2, 10, 10)), torch.ones((2, 10, 10))
-        pred[0, :, :] = 0
-        val = dice(pred, target)
-        self.assertIsInstance(val, torch.Tensor)
-        self.assertTrue(torch.allclose(val, torch.tensor([0, 1], dtype=torch.float32)))
-        self.assertEqual(pred.size(), torch.Size([2, 10, 10]))
-        self.assertEqual(target.size(), torch.Size([2, 10, 10]))
-
-        pred, target = torch.ones((2, 3, 10, 10)), torch.ones((2, 3, 10, 10))
-        pred[0, 0, :5, :] = 0
-        target[0, 0, :, :5] = 0
-        val = multiclass_dice(pred, target)
-        self.assertIsInstance(val, torch.Tensor)
-        self.assertTrue(torch.allclose(val, torch.tensor([[0.5, 1], [1, 1], [1, 1]], dtype=torch.float32)))
-        self.assertEqual(pred.size(), torch.Size([2, 3, 10, 10]))
-        self.assertEqual(target.size(), torch.Size([2, 3, 10, 10]))
-
-        pred, target = torch.zeros((1, 10, 10)), torch.zeros((1, 10, 10))
-        self.assertTrue(torch.allclose(dice(pred, target), torch.tensor([1], dtype=torch.float32)))
-
-        pred[0, :5, :] = 1
-        target[0, :, :5] = 1
-        val = dice(pred, target)
-        self.assertTrue(torch.allclose(val, torch.tensor([0.5], dtype=torch.float32)))
+        self._test_metric(dice, multiclass_dice, 0.5, self._test_functional_metric)
 
     def test_jaccard_dice_converting(self):
         pred, target = torch.ones((1, 10, 10)), torch.ones((1, 10, 10))
@@ -96,3 +39,59 @@ class PyTorchTest(unittest.TestCase):
         pred, target = torch.zeros((2, 3, 10, 10)), torch.zeros((2, 3, 10, 10))
         self.assertTrue(torch.allclose(multiclass_dice(pred, target), jaccard2dice(multiclass_jaccard(pred, target))))
         self.assertTrue(torch.allclose(multiclass_jaccard(pred, target), dice2jaccard(multiclass_dice(pred, target))))
+
+    def test_dice_metric(self):
+        self._test_metric(DiceMetric().calc, MulticlassDiceMetric(reduction=None).calc, 0.5, self._test_class_metric,
+                          multiclass_rediction=None)
+        self._test_metric(DiceMetric().calc, MulticlassDiceMetric(reduction='sum').calc, 0.5, self._test_class_metric,
+                          multiclass_rediction='sum')
+        self._test_metric(DiceMetric().calc, MulticlassDiceMetric(reduction='mean').calc, 0.5, self._test_class_metric,
+                          multiclass_rediction='mean')
+
+        self._test_metric(JaccardMetric().calc, MulticlassJaccardMetric(reduction=None).calc, 1 / 3, self._test_class_metric,
+                          multiclass_rediction=None)
+        self._test_metric(JaccardMetric().calc, MulticlassJaccardMetric(reduction='sum').calc, 1 / 3, self._test_class_metric,
+                          multiclass_rediction='sum')
+        self._test_metric(JaccardMetric().calc, MulticlassJaccardMetric(reduction='mean').calc, 1 / 3, self._test_class_metric,
+                          multiclass_rediction='mean')
+
+    def _test_functional_metric(self, func: callable, pred: Tensor, target: Tensor, expected_res: Tensor):
+        pred_size, target_size = pred.size(), target.size()
+        val = func(pred, target)
+        self.assertIsInstance(val, torch.Tensor)
+        self.assertTrue(torch.allclose(val, expected_res))
+        self.assertEqual(pred.size(), pred_size)
+        self.assertEqual(target.size(), target_size)
+
+    def _test_class_metric(self, func: callable, pred: Tensor, target: Tensor, expected_res: Tensor):
+        pred_size, target_size = pred.shape, target.shape
+        val = func(pred, target)
+        self.assertTrue(type(val) in [float, np.ndarray])
+        self.assertTrue(np.allclose(val, expected_res))
+        self.assertEqual(pred.shape, pred_size)
+        self.assertEqual(target.shape, target_size)
+
+    def _test_metric(self, func: callable, multiclass_func: callable, res_for_half: float, test_method: callable, multiclass_rediction: str = None):
+        test_method(func, torch.ones((1, 10, 10)), torch.ones((1, 10, 10)), torch.FloatTensor([1]))
+
+        pred, target = torch.ones((2, 10, 10)), torch.ones((2, 10, 10))
+        pred[0, :, :] = 0
+        test_method(func, pred, target, torch.FloatTensor([0, 1]))
+
+        pred, target = torch.ones((2, 3, 10, 10)), torch.ones((2, 3, 10, 10))
+        pred[0, 0, :5, :] = 0
+        target[0, 0, :, :5] = 0
+        if multiclass_rediction is None:
+            expected_res = torch.FloatTensor([[res_for_half, 1], [1, 1], [1, 1]])
+        elif multiclass_rediction == 'sum':
+            expected_res = torch.FloatTensor([res_for_half, 1]) + torch.FloatTensor([1, 1]) + torch.FloatTensor([1, 1])
+        elif multiclass_rediction == 'mean':
+            expected_res = torch.FloatTensor([[res_for_half, 1], [1, 1], [1, 1]]).mean(0)
+        test_method(multiclass_func, pred, target, expected_res)
+
+        pred, target = torch.zeros((1, 10, 10)), torch.zeros((1, 10, 10))
+        test_method(func, torch.zeros((1, 10, 10)), torch.zeros((1, 10, 10)), torch.FloatTensor([1]))
+
+        pred[0, :5, :] = 1
+        target[0, :, :5] = 1
+        test_method(func, pred, target, torch.FloatTensor([res_for_half]))