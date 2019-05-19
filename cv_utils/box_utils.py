fix losses calculation; add correct tests for losses; start to implement f-beta score on boxes

cv_utils/box_utils.py
@@ -5,10 +5,10 @@ def calc_boxes_areas(boxes: np.ndarray):
     """
     Calculate areas of array of boxes
 
-    :param boxes: array of boxes with shape [B, N, 4]
-    :return: array of boxes area with shape [B, N]
+    :param boxes: array of boxes with shape [N, 4]
+    :return: array of boxes area with shape [N]
     """
-    xx, yy = np.take(boxes, [0, 2], axis=2), np.take(boxes, [1, 3], axis=2)  # [B, N, 2], [B, N, 2]
-    boxes_x_min, boxes_x_max = xx.min(2), xx.max(2)  # [B, N], [B, N]
-    boxes_y_min, boxes_y_max = yy.min(2), yy.max(2)  # [B, N], [B, N]
-    return (boxes_x_max - boxes_x_min) * (boxes_y_max - boxes_y_min)  # [B, N], [B, N]
+    xx, yy = np.take(boxes, [0, 2], axis=1), np.take(boxes, [1, 3], axis=1)  # [N, 2], [N, 2]
+    boxes_x_min, boxes_x_max = xx.min(1), xx.max(1)  # [N], [N]
+    boxes_y_min, boxes_y_max = yy.min(1), yy.max(1)  # [N], [N]
+    return (boxes_x_max - boxes_x_min) * (boxes_y_max - boxes_y_min)  # [N], [N]

cv_utils/losses/segmentation.py
@@ -3,7 +3,7 @@ from torch import Tensor
 from torch.nn import Module
 
 from cv_utils.losses.common import ComposedLoss, Reduction
-from cv_utils.metrics.torch.segmentation import dice, _multiclass_metric
+from cv_utils.metrics.torch.segmentation import dice, _multiclass_metric, _split_masks_by_classes
 from cv_utils.models.utils import Activation
 
 
@@ -15,10 +15,10 @@ class DiceLoss(Module):
         eps (float): smooth value. When eps == 1 it's named Smooth Dice loss
         activation (srt): the activation function, that applied to predicted values. See :class:`Activation` for possible values
     """
-    def __init__(self, eps: float = 1, activation: str = None, reduction: Reduction = None):
+    def __init__(self, eps: float = 1, activation: str = None, reduction: Reduction = Reduction('sum')):
         super().__init__()
         self._activation = Activation(activation)
-        self._reduction = lambda x: x if reduction is None else reduction
+        self._reduction = reduction
         self._eps = eps
 
     def forward(self, output: Tensor, target: Tensor):
@@ -50,10 +50,13 @@ class MulticlassSegmentationLoss(Module):
     Args:
          base_loss (Module): basic loss object
     """
-    def __init__(self, base_loss: Module, reduction: Reduction = None):
+    def __init__(self, base_loss: Module, reduction: Reduction = Reduction('sum')):
         super().__init__()
         self._base_loss = base_loss
-        self._reduction = lambda x: x if reduction is None else reduction
+        self._reduction = reduction
 
-    def forward(self, output, target):
-        return self._reduction(_multiclass_metric(self._base_loss, output, target))
+    def forward(self, output: Tensor, target: Tensor):
+        res = torch.zeros((output.size(1)), dtype=output.dtype)
+        for i, [p, t] in enumerate(_split_masks_by_classes(output, target)):
+            res[i] = self._base_loss(torch.squeeze(p, dim=1), torch.squeeze(t, dim=1))
+        return self._reduction(res)

cv_utils/metrics/cpu/detection.py
@@ -3,23 +3,26 @@ import numpy as np
 from cv_utils.box_utils import calc_boxes_areas
 
 
-def _compute_boxes_iou(box: np.ndarray or [], boxes: np.ndarray or [], box_area: float, boxes_area: float):
+def _compute_boxes_iou(box: np.ndarray or [], boxes: np.ndarray or [], box_area: float, boxes_area: [float]) -> np.ndarray:
     """
     Calculates IoU of the given box with the array of the given boxes.
 
     Args:
         box: 1D vector [y1, x1, y2, x2]
-        boxes: [boxes_count, (y1, x1, y2, x2)]
+        boxes: [batch, boxes_count, (y1, x1, y2, x2)]
         box_area: float. the area of 'box'
-        boxes_area: array of length boxes_count.
+        boxes_area: array of boxes areas with shape [batch, boxes_count].
 
     Note: the areas are passed in rather than calculated here for
           efficency. Calculate once in the caller to avoid duplicate work.
+
+    Returns:
+        array of iou in shape []
     """
-    xmin = np.maximum(box[0], boxes[:, :, 0])
-    ymin = np.maximum(box[1], boxes[:, :, 1])
-    xmax = np.minimum(box[2], boxes[:, :, 2])
-    ymax = np.minimum(box[3], boxes[:, :, 3])
+    xmin = np.maximum(box[0], boxes[:, 0])
+    ymin = np.maximum(box[1], boxes[:, 1])
+    xmax = np.minimum(box[2], boxes[:, 2])
+    ymax = np.minimum(box[3], boxes[:, 3])
     intersection = (xmax - xmin) * (ymax - ymin)
     union = box_area + boxes_area[:] - intersection[:]
     iou = intersection / union
@@ -39,12 +42,13 @@ def f_beta_score(pred: np.ndarray, target: np.ndarray, beta: int, thresholds: [f
     Returns:
         np.ndarray: array with values of F-Beta score. Array shape: [B]
     """
-    pred_areas = calc_boxes_areas(pred)  # [B, N], [B, N]
-    target_areas = calc_boxes_areas(target)  # [B, N], [B, N]
-
     ious = []
-    for batch_idx in enumerate(pred.shape[0]):
-        for instance_idx in enumerate(pred.shape[1]):
-            _compute_boxes_iou(pred[batch_idx][instance_idx], target[batch_idx], pred_areas[batch_idx][instance_idx], target_areas[batch_idx])
+    for batch_idx in range(pred.shape[0]):
+        pred_areas = calc_boxes_areas(pred[batch_idx])  # [N], [N]
+        target_areas = calc_boxes_areas(target[batch_idx])  # [N], [N]
+        for instance_idx in range(pred.shape[1]):
+            ious.append(_compute_boxes_iou(pred[batch_idx][instance_idx], target[batch_idx], pred_areas[instance_idx], target_areas))
 
+        for thresh in thresholds:
+            pass
     raise NotImplementedError()

tests/box_utils.py
@@ -8,7 +8,7 @@ __all__ = ['BoxUtilstest']
 
 class BoxUtilstest(unittest.TestCase):
     def test_calc_boxes_areas(self):
-        boxes = np.array([[[1, 1, 2, 2], [3, 3, 5, 5]], [[1, 3, 2, 5], [-1, -3, 2, 5]]])
+        boxes = np.array([[1, 1, 2, 2], [3, 3, 5, 5], [1, 3, 2, 5], [-1, -3, 2, 5]])
         res = calc_boxes_areas(boxes)
         self.assertIsInstance(res, np.ndarray)
-        self.assertTrue(np.allclose(res, [[1, 4], [2, 24]]))
+        self.assertTrue(np.allclose(res, [1, 4, 2, 24]))

tests/losses/segmentation.py
@@ -2,6 +2,7 @@ import unittest
 import torch
 from torch import Tensor
 
+from cv_utils.losses.common import Reduction
 from cv_utils.losses.segmentation import DiceLoss, MulticlassSegmentationLoss
 from cv_utils.metrics.torch.segmentation import dice, multiclass_dice
 
@@ -10,65 +11,53 @@ __all__ = ['SegmentationLossesTest']
 
 class SegmentationLossesTest(unittest.TestCase):
     def test_dice(self):
-        pred, target = torch.ones((1, 10, 10)), torch.ones((1, 10, 10))
+        for batch_reduction in ['sum', 'mean']:
+            for multiclass_reduction in ['sum', 'mean']:
+                for eps in [1, 1e-7]:
+                    with self.subTest(batch_reduction=batch_reduction, multiclass_reduction=multiclass_reduction, eps=eps):
+                        self._test_loss(DiceLoss(eps=eps, reduction=Reduction(batch_reduction)),
+                                        MulticlassSegmentationLoss(DiceLoss(eps=eps, reduction=Reduction(batch_reduction)),
+                                                                   reduction=Reduction(multiclass_reduction)),
+                                        batch_reduction=batch_reduction, multiclass_rediction=multiclass_reduction, eps=eps)
 
-        true_val = dice(pred, target, eps=1)
-        res = DiceLoss(eps=1, activation=None)(pred, target)
-        self.assertTrue(torch.allclose(true_val, 1 - res))
-
-        true_val = dice(pred, target, eps=1e-7)
-        res = DiceLoss(eps=1e-7, activation=None)(pred, target)
-        self.assertTrue(torch.allclose(true_val, 1 - res))
-        self.assertIsInstance(res, torch.Tensor)
-        self.assertEqual(res.size(), torch.Size([1]))
-
-        pred[0, :5, :] = 0
-        target[0, :, :5] = 0
-        val = DiceLoss(eps=1e-7, activation=None)(pred, target)
-        self.assertTrue(torch.allclose(val, 1 - torch.tensor([0.5], dtype=torch.float32)))
-        self.assertIsInstance(res, torch.Tensor)
-        self.assertEqual(res.size(), torch.Size([1]))
-
-    def test_multiclass_loss(self):
-        pred, target = torch.ones((2, 10, 10)), torch.ones((2, 10, 10))
-        true_val = dice(pred, target, eps=1e-7)
-        res = DiceLoss(eps=1e-7, activation=None)(pred, target)
-        self.assertTrue(torch.allclose(true_val, 1 - res))
-
-        pred, target = torch.ones((2, 3, 10, 10)), torch.ones((2, 3, 10, 10))
-        true_val = multiclass_dice(pred, target, eps=1)
-        res = MulticlassSegmentationLoss(DiceLoss(eps=1, activation=None))(pred, target)
-        self.assertTrue(torch.allclose(true_val, 1 - res))
-
-    def _test_class_metric(self, func: callable, pred: Tensor, target: Tensor, expected_res: Tensor):
+    def _test_loss_case(self, func: callable, pred: Tensor, target: Tensor, expected_res: Tensor):
         pred_size, target_size = pred.size(), target.size()
         val = func(pred, target)
         self.assertIsInstance(val, Tensor)
+        self.assertEqual(val.size(), torch.Size([]))
         self.assertTrue(torch.allclose(val, expected_res))
         self.assertEqual(pred.size(), pred_size)
         self.assertEqual(target.size(), target_size)
 
-    def _test_metric(self, func: callable, multiclass_func: callable, res_for_half: float, test_method: callable, multiclass_rediction: str = None):
-        test_method(func, torch.ones((1, 10, 10)), torch.ones((1, 10, 10)), torch.FloatTensor([1]))
+    def _test_loss(self, func: callable, multiclass_func: callable, batch_reduction: str, multiclass_rediction: str, eps: float):
+        self._test_loss_case(func, torch.ones((1, 10, 10)), torch.ones((1, 10, 10)), torch.FloatTensor([0]))
 
         pred, target = torch.ones((2, 10, 10)), torch.ones((2, 10, 10))
         pred[0, :, :] = 0
-        test_method(func, pred, target, torch.FloatTensor([0, 1]))
+        expected_res = 1 - dice(pred, target, eps=eps)
+        if batch_reduction == 'sum':
+            expected_res = expected_res.sum()
+        elif batch_reduction == 'mean':
+            expected_res = expected_res.mean()
+        self._test_loss_case(func, pred, target, expected_res)
 
         pred, target = torch.ones((2, 3, 10, 10)), torch.ones((2, 3, 10, 10))
         pred[0, 0, :5, :] = 0
         target[0, 0, :, :5] = 0
-        if multiclass_rediction is None:
-            expected_res = torch.FloatTensor([[res_for_half, 1], [1, 1], [1, 1]])
-        elif multiclass_rediction == 'sum':
-            expected_res = torch.FloatTensor([res_for_half, 1]) + torch.FloatTensor([1, 1]) + torch.FloatTensor([1, 1])
+        expected_res = 1 - multiclass_dice(pred, target, eps=eps)
+        if batch_reduction == 'sum':
+            expected_res = expected_res.sum(1)
+        elif batch_reduction == 'mean':
+            expected_res = expected_res.mean(1)
+        if multiclass_rediction == 'sum':
+            expected_res = expected_res.sum(0)
         elif multiclass_rediction == 'mean':
-            expected_res = torch.FloatTensor([[res_for_half, 1], [1, 1], [1, 1]]).mean(0)
-        test_method(multiclass_func, pred, target, expected_res)
+            expected_res = expected_res.mean(0)
+        self._test_loss_case(multiclass_func, pred, target, expected_res)
 
         pred, target = torch.zeros((1, 10, 10)), torch.zeros((1, 10, 10))
-        test_method(func, torch.zeros((1, 10, 10)), torch.zeros((1, 10, 10)), torch.FloatTensor([1]))
+        self._test_loss_case(func, torch.zeros((1, 10, 10)), torch.zeros((1, 10, 10)), 1 - dice(pred, target, eps=eps))
 
         pred[0, :5, :] = 1
         target[0, :, :5] = 1
-        test_method(func, pred, target, torch.FloatTensor([res_for_half]))
+        self._test_loss_case(func, pred, target, 1 - dice(pred, target, eps=eps))

tests/metrics/cpu/detection.py
@@ -2,20 +2,22 @@ import unittest
 import numpy as np
 
 from cv_utils.box_utils import calc_boxes_areas
-from cv_utils.metrics.cpu.detection import _compute_boxes_iou
+from cv_utils.metrics.cpu.detection import _compute_boxes_iou, f_beta_score
 
 __all__ = ['CPUTest']
 
 
 class CPUTest(unittest.TestCase):
     def test_boxex_iou(self):
-        pred = np.array([[[0, 1, 3, 3]]])
-        target = np.array([[[1, 1, 2, 2], [3, 3, 5, 5]], [[1, 3, 2, 5], [-1, -3, 2, 5]]])
+        pred = np.array([[0, 1, 3, 3]])
+        target = np.array([[1, 1, 2, 2], [3, 3, 5, 5], [1, 3, 2, 5], [-1, -3, 2, 5]])
         pred_areas = calc_boxes_areas(pred)
         target_areas = calc_boxes_areas(target)
-        res = _compute_boxes_iou(pred[0][0], target, pred_areas[0][0], target_areas)
-        self.assertTrue(np.allclose(res, [[1 / 6, 0], [0, 4 / 26]]))
+        res = _compute_boxes_iou(pred[0], target, pred_areas[0], target_areas)
+        self.assertTrue(np.allclose(res, [1 / 6, 0, 0, 4 / 26]))
 
-    # def test_f_beta(self):
-    #     preds = np.array([[[1, 1, 2, 2], [3, 3, 5, 5]], [[1, 3, 2, 5]]])
-    #     target = np.array([[[1, 1, 2, 2], [3, 3, 5, 5]], [[1, 3, 2, 5]]]) - np.array([1, 1])
+    def test_f_beta(self):
+        preds = np.array([[[1, 1, 2, 2], [3, 3, 5, 5]], [[1, 3, 2, 5], [-1, -3, 2, 5]]])
+        target = np.array([[[1, 1, 2, 2], [3, 3, 5, 5]], [[1, 3, 2, 5], [-1, -3, 2, 5]]]) - np.array([1, 1, 1, 1])
+
+        res = f_beta_score(preds, target, beta=2, thresholds=[0.5, 0.9])