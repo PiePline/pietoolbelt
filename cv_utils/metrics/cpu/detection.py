010242e23004df1b716c097f1505f8ef2c0db7c5
toodef <anton.fedotov@lge.com>
2019-05-21 12:46:20 +0300

    correct f-beta and calc_tp_fp_fn

--- cv_utils/metrics/cpu/detection.py
+++ cv_utils/metrics/cpu/detection.py
@@ -9,7 +9,7 @@ def _compute_boxes_iou(box: np.ndarray or [], boxes: np.ndarray or [], box_area:
 
     Args:
         box: 1D vector [y1, x1, y2, x2]
-        boxes: [batch, boxes_count, (y1, x1, y2, x2)]
+        boxes: [boxes_count, (y1, x1, y2, x2)]
         box_area: float. the area of 'box'
         boxes_area: array of boxes areas with shape [batch, boxes_count].
 
@@ -24,9 +24,39 @@ def _compute_boxes_iou(box: np.ndarray or [], boxes: np.ndarray or [], box_area:
     xmax = np.minimum(box[2], boxes[:, 2])
     ymax = np.minimum(box[3], boxes[:, 3])
     intersection = (xmax - xmin) * (ymax - ymin)
+    intersection[xmin > xmax] = 0
+    intersection[ymin > ymax] = 0
+    intersection[intersection < 0] = 0
     union = box_area + boxes_area[:] - intersection[:]
     iou = intersection / union
     return iou
+
+
+def calc_tp_fp_fn(pred: np.ndarray, target: np.ndarray, threshold: float):
+    """
+    Calculate true positives, false positives and false negatives number for predicted and target boxes
+
+    Args:
+        pred: Array of predicted boxes with shape [N, 4]
+        target: Array of ground truth boxes with shape [N, 4]
+        threshold: the threshold for iou metric
+
+    Returns:
+        Return list of [tp, fp, fn]
+    """
+    pred_areas = calc_boxes_areas(pred)  # [N], [N]
+    target_areas = calc_boxes_areas(target)  # [N], [N]
+    ious = []
+    for instance_idx in range(pred.shape[0]):
+        ious.append(_compute_boxes_iou(pred[instance_idx], target, pred_areas[instance_idx], target_areas))
+
+    matches_matrix = np.array(ious)
+    matches_matrix[matches_matrix < threshold] = 0
+    tp = matches_matrix[matches_matrix > 0].shape[0]
+    fn = target.shape[0] - tp
+    fp = pred.shape[0] - tp
+
+    return tp, fn, fp
 
 
 def f_beta_score(pred: np.ndarray, target: np.ndarray, beta: int, thresholds: [float]):
@@ -43,21 +73,15 @@ def f_beta_score(pred: np.ndarray, target: np.ndarray, beta: int, thresholds: [f
     Returns:
         np.ndarray: array with values of F-Beta score. Array shape: [B]
     """
+    beta_squared = beta ** 2
+    res = []
     for batch_idx in range(pred.shape[0]):
+        batch_results = []
         for thresh in thresholds:
-            tp, fp, fn = 0, 0, 0
-            pred_areas = calc_boxes_areas(pred[batch_idx])  # [N], [N]
-            target_areas = calc_boxes_areas(target[batch_idx])  # [N], [N]
-            ious = []
-            for instance_idx in range(pred.shape[1]):
-                ious.append(_compute_boxes_iou(pred[batch_idx][instance_idx], target[batch_idx], pred_areas[instance_idx], target_areas))
+            tp, fp, fn = calc_tp_fp_fn(pred[batch_idx], target[batch_idx], thresh)
+            precision = tp / (tp + fp)
+            recall = tp / (tp + fn)
+            batch_results.append((beta_squared + 1) * (precision * recall) / (beta_squared * precision + recall + 1e-7))
 
-            confusion_matrix = np.array(ious)
-            matched_num = np.clip(iou[iou >= thresh], 0, 1).sum()
-
-            if matched_num > 1:
-                tp += 1
-            else:
-                fn += 1
-
-    raise NotImplementedError()
+        res.append(np.mean(batch_results))
+    return np.array(res)
--- tests/metrics/cpu/detection.py
+++ tests/metrics/cpu/detection.py
@@ -2,7 +2,7 @@ import unittest
 import numpy as np
 
 from cv_utils.box_utils import calc_boxes_areas
-from cv_utils.metrics.cpu.detection import _compute_boxes_iou, f_beta_score
+from cv_utils.metrics.cpu.detection import _compute_boxes_iou, f_beta_score, calc_tp_fp_fn
 
 __all__ = ['CPUTest']
 
@@ -16,9 +16,40 @@ class CPUTest(unittest.TestCase):
         res = _compute_boxes_iou(pred[0], target, pred_areas[0], target_areas)
         self.assertTrue(np.allclose(res, [1 / 6, 0, 0, 4 / 26]))
 
-    def test_f_beta(self):
-        preds = np.array([[[1, 1, 2, 2], [3, 3, 5, 5]], [[1, 3, 2, 5], [-1, -3, 2, 5]]])
-        target = np.array([[[1, 1, 2, 2], [3, 3, 5, 5]], [[1, 3, 2, 5], [-1, -3, 2, 5]]]) - np.array([1, 1, 1, 1])
+        pred = np.array([[3, 0, 5, 2], [3.5, 3.5, 5.5, 5.5]])
+        target = np.array([[0, 0, 2, 2], [3, 3, 5, 5]])
+        pred_areas = calc_boxes_areas(pred)
+        target_areas = calc_boxes_areas(target)
+        res = _compute_boxes_iou(pred[0], target, pred_areas[0], target_areas)
+        self.assertTrue(np.allclose(res, [0, 0]))
+        res = _compute_boxes_iou(pred[1], target, pred_areas[0], target_areas)
+        self.assertTrue(np.allclose(res, [0, 2.25 / 5.75]))
 
-        res = f_beta_score(preds, target, beta=2, thresholds=[0.5, 0.9])
-        print('a')
+    def test_tp_fp_fn(self):
+        preds = np.array([[3, 0, 5, 2], [3.5, 3.5, 5.5, 5.5]])
+        target = np.array([[0, 0, 2, 2], [3, 3, 5, 5]])
+        tp, fp, fn = 1, 1, 1
+
+        res = calc_tp_fp_fn(preds, target, threshold=0.1)
+        self.assertEqual(res, (tp, fp, fn))
+
+        res = calc_tp_fp_fn(preds, target, threshold=2.25 / 5.75)
+        self.assertEqual(res, (tp, fp, fn))
+
+        res = calc_tp_fp_fn(preds, target, threshold=2.25 / 5.75 + 1e-6)
+        self.assertEqual(res, (0, 2, 2))
+
+    def test_f_beta(self):
+        preds = np.array([[[3, 0, 5, 2], [3.5, 3.5, 5.5, 5.5]]])
+        target = np.array([[[0, 0, 2, 2], [3, 3, 5, 5]]])
+
+        beta = 1
+        tp, fp, fn = 1, 1, 1
+        precision = tp / (tp + fp)
+        recall = tp / (tp + fn)
+
+        beta_squared = beta ** 2
+        expected_res = (beta_squared + 1) * (precision * recall) / (beta_squared * precision + recall + 1e-7)
+
+        res = f_beta_score(preds, target, beta=2, thresholds=[0.1])
+        self.assertAlmostEqual(res, expected_res, delta=1e-6)
