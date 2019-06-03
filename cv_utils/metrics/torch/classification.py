eaaf6ffd020c66750a4697a808ebfae8e0f7403f
toodef <anton.fedotov@lge.com>
2019-06-03 16:50:28 +0300

    add some classification metrics; fix ROCAUC metric

--- cv_utils/metrics/torch/classification.py
+++ cv_utils/metrics/torch/classification.py
@@ -1,49 +1,28 @@
+from abc import abstractmethod, ABCMeta
+
 import numpy as np
 from neural_pipeline import MetricsProcessor, MetricsGroup, AbstractMetric
 from torch import Tensor
-from sklearn.metrics import roc_auc_score
+from sklearn.metrics import roc_auc_score, confusion_matrix
 
 __all__ = ['ROCAUCMetric', 'ClassificationMetricsProcessor']
 
 
-class ROCAUCMetric(AbstractMetric):
-    def __init__(self, thresold: float, name: str = 'ROC_AUC'):
+class _ClassificationMetric(AbstractMetric):
+    def __init__(self, name: str):
         super().__init__(name)
-        self._thresh = thresold
+
         self._targets, self._preds = [], []
         self._pred_preprocess = lambda x: x.data.cpu().numpy()
         self._target_preprocess = lambda x: x.data.cpu().numpy()
 
-    def set_pred_preproc(self, preproc: callable) -> 'ROCAUCMetric':
+    def set_pred_preproc(self, preproc: callable) -> '_ClassificationMetric':
         self._pred_preprocess = preproc
         return self
 
-    def set_target_preproc(self, preproc: callable) -> 'ROCAUCMetric':
+    def set_target_preproc(self, preproc: callable) -> '_ClassificationMetric':
         self._target_preprocess = preproc
         return self
-
-    def calc(self, predict: Tensor, target: Tensor) -> np.ndarray or float:
-        """
-        Calc metric
-
-        Args:
-            predict (Tensor): predict classes as Tensor of size [B, C]
-            target (Tensor): ground truth classes as Tensor of size [B, C]
-        Returns:
-             return zero cause metric accumulate all values and calc when :meth:`get_values`
-        """
-        pred = self._pred_preprocess(predict)
-        tar = self._target_preprocess(target)
-
-        pred[pred < self._thresh] = 0
-        self._preds.extend(list(pred))
-        self._targets.extend(list(tar))
-        return 0
-
-    def get_values(self):
-        res = roc_auc_score(np.squeeze(self._targets), np.squeeze(self._preds))
-        self._targets, self._preds = [], []
-        return res
 
     @staticmethod
     def multiclass_pred_preproc(val: Tensor):
@@ -67,7 +46,7 @@ class ROCAUCMetric(AbstractMetric):
               val (Tensor): values to preprocess as Tensor of size [B, C]
 
         Returns:
-            np.ndarray of shape [B, 1]
+            np.ndarray of shape [B]
         """
         val_internal = val.data.cpu().numpy()
         idx = np.argmax(val_internal, axis=1)
@@ -86,7 +65,78 @@ class ROCAUCMetric(AbstractMetric):
             np.ndarray of shape [B, 1]
         """
         val_internal = val.data.cpu().numpy()
-        return (val_internal > 0).astype(np.int)
+        return np.clip(val_internal, 0, 1).astype(np.int)
+
+    def calc(self, predict: Tensor, target: Tensor) -> np.ndarray or float:
+        """
+        Calc metric
+
+        Args:
+            predict (Tensor): predict classes as Tensor of size [B, C]
+            target (Tensor): ground truth classes as Tensor of size [B, C]
+        Returns:
+             return zero cause metric accumulate all values and calc when :meth:`get_values`
+        """
+        pred = self._pred_preprocess(predict)
+        tar = self._target_preprocess(target)
+
+        self._preds.extend(pred)
+        self._targets.extend(tar)
+        return 0
+
+    def _calc(self, output: Tensor, target: Tensor):
+        self.calc(output, target)
+
+    @abstractmethod
+    def _get_values(self):
+        pass
+
+    def get_values(self):
+        """
+        Get values of metric
+        :return:
+        """
+        res = self._get_values()
+        self._targets, self._preds = [], []
+        return res
+
+
+class ROCAUCMetric(_ClassificationMetric):
+    def __init__(self, thresold: float, name: str = 'ROC_AUC'):
+        super().__init__(name)
+        self._thresh = thresold
+
+    def _get_values(self):
+        preds = np.where(np.squeeze(self._preds) < self._thresh, 0, 1)
+
+        try:
+            res = roc_auc_score(np.squeeze(self._targets), preds)
+        except ValueError:
+            return np.nan
+
+        return res
+
+
+class RecallMetric(_ClassificationMetric):
+    def __init__(self, threshold: float, name: str = "Recall"):
+        super().__init__(name)
+        self._thresh = threshold
+
+    def _get_values(self):
+        preds = np.where(np.squeeze(self._preds) < self._thresh, 0, 1)
+        tn, fp, fn, tp = confusion_matrix(np.squeeze(self._targets), preds).ravel()
+        return tp / (tp + fn)
+
+
+class ActCMetric(_ClassificationMetric):
+    def __init__(self, threshold: float, name: str = "ActC"):
+        super().__init__(name)
+        self._thresh = threshold
+
+    def _get_values(self):
+        preds = np.where(np.squeeze(self._preds) < self._thresh, 0, 1)
+        tn, fp, fn, tp = confusion_matrix(np.squeeze(self._targets), preds).ravel()
+        return fp / (fp + tn) + 19 * fn / (fn + tp)
 
 
 class ClassificationMetricsProcessor(MetricsProcessor):
--- tests/metrics/torch/classification.py
+++ tests/metrics/torch/classification.py
@@ -11,17 +11,20 @@ __all__ = ['PyTorchTest']
 
 class PyTorchTest(unittest.TestCase):
     def test_standard_case(self):
-        out = torch.rand(10, 1)
-        target = torch.ones(10, 1)
-        target[0][0] = 0
+        for _ in range(10):
+            out = torch.rand(10, 1)
+            target = torch.ones(10, 1)
+            target[0][0] = 0
+            thresh = 0.5
 
-        true_res = roc_auc_score(target.numpy().astype(np.int), out.numpy())
+            out_for_res = np.where(out.numpy() < thresh, 0, 1)
+            true_res = roc_auc_score(target.numpy().astype(np.int), out_for_res)
 
-        metric = ROCAUCMetric(0.5)
-        metric.calc(out, target)
-        res = metric.get_values()
+            metric = ROCAUCMetric(thresh)
+            metric.calc(out, target)
+            res = metric.get_values()
 
-        self.assertTrue(np.allclose(res, true_res), msg="res: {}, true_res: {}".format(res, true_res))
+            self.assertTrue(np.allclose(res, true_res), msg="res: {}, true_res: {}".format(res, true_res))
 
     def test_builtin_preproc_methods(self):
         out = torch.Tensor([[0.1, 0.5, 0.3],
@@ -37,15 +40,22 @@ class PyTorchTest(unittest.TestCase):
         self.assertTrue(np.allclose(res, [[0], [1], [1], [1], [0]]))
 
     def test_multiclass_case(self):
-        pred = torch.rand(10, 4)
-        target = torch.from_numpy((np.random.rand(10, 1) * 4).astype(np.int))
+        for _ in range(10):
+            pred = torch.rand(10, 4)
+            target = torch.from_numpy((np.random.rand(10, 1) * 4).astype(np.int))
+            thresh = 0.7
 
-        true_res = roc_auc_score(ROCAUCMetric.multiclass_target_preproc(target), ROCAUCMetric.multiclass_pred_preproc(pred))
+            metric = ROCAUCMetric(thresh)\
+                .set_pred_preproc(ROCAUCMetric.multiclass_pred_preproc)\
+                .set_target_preproc(ROCAUCMetric.multiclass_target_preproc)
+            metric.calc(pred, target)
+            res = metric.get_values()
 
-        metric = ROCAUCMetric(0.5)\
-            .set_pred_preproc(ROCAUCMetric.multiclass_pred_preproc)\
-            .set_target_preproc(ROCAUCMetric.multiclass_target_preproc)
-        metric.calc(pred, target)
-        res = metric.get_values()
+            if target.max() < 1 or target.min() > 0:
+                self.assertIs(res, np.nan)
+            else:
+                out_for_res = ROCAUCMetric.multiclass_pred_preproc(pred)
+                out_for_res = np.where(out_for_res < thresh, 0, 1)
+                true_res = roc_auc_score(ROCAUCMetric.multiclass_target_preproc(target), out_for_res)
 
-        self.assertTrue(np.allclose(res, true_res), msg="res: {}, true_res: {}".format(res, true_res))
+                self.assertTrue(np.allclose(res, true_res), msg="res: {}, true_res: {}".format(res, true_res))
