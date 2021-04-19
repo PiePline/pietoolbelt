2f28164b9be624c463f419a0cbddbcf718a02a61
toodef <fedotov.anton.af@gmail.com>
2021-04-19 20:00:13 +0300

    add bagging result class; improve bagging testing

--- pietoolbelt/pipeline/bagging/common.py
+++ pietoolbelt/pipeline/bagging/common.py
@@ -1,4 +1,6 @@
 import itertools
+import json
+import os
 from typing import Callable, List, Any, Dict
 
 import numpy as np
@@ -13,29 +15,78 @@ __all__ = ['BasicBagging', 'BaggingResult']
 class BaggingResult(AbstractStepDirResult):
     def __init__(self, path: str):
         super().__init__(path)
+        self._res_file = os.path.join(path, 'meta.json')
 
-    def set_config(self, config: dict):
-        raise NotImplementedError()
+    def add_cmb(self, combination: List[str], err: float):
+        prev_data = self._load_prev_data()
+
+        if 'all_cmb' in prev_data:
+            for res in prev_data['all_cmb']:
+                if combination == res['cmb']:
+                    raise IndexError("Combination already in result file. Combination: {}".format(combination))
+
+        prev_data['all_cmb'].append({'cmb': combination, 'err': err})
+        with open(self._res_file, 'w') as res_file:
+            json.dump(prev_data, res_file)
+
+    def set_result(self, combination: List[str], err: float):
+        prev_data = self._load_prev_data()
+
+        if 'res' in prev_data:
+            raise Exception("Result already specified")
+
+        prev_data['res'] = {'cmb': combination, 'err': err}
+
+        with open(self._res_file, 'w') as res_file:
+            json.dump(prev_data, res_file)
+
+    def _load_prev_data(self) -> dict:
+        if os.path.exists(self._res_file):
+            with open(self._res_file, 'r+') as res_file:
+                prev_data = json.load(res_file)
+        else:
+            prev_data = dict()
+
+        if 'all_cmb' not in prev_data:
+            prev_data['all_cmb'] = []
+
+        return prev_data
 
 
 class BasicBagging:
     def __init__(self, predicts_result: Dict[str, AbstractPredictResult], calc_error: Callable[[float, float], float],
-                 dataset: BasicDataset, reduce: Callable[[List[float]], float]):
+                 dataset: BasicDataset, reduce: Callable[[List[float]], float], result: BaggingResult):
+        if len(predicts_result) < 2:
+            raise Exception("Predicts results number must be >= 1")
+
         self._predicts_results = predicts_result
         self._dataset = dataset
+        self._result = result
 
         self._calc_error = calc_error
         self._reduce = reduce
 
-    def run(self, max_cmb_len: int = None) -> List[AbstractPredictResult]:
+        self._pick_target = lambda x: x['target'].numpy()
+
+    def set_pick_target(self, pick: Callable[[Any], Any]) -> 'BasicBagging':
+        self._pick_target = pick
+        return self
+
+    def run(self, max_cmb_len: int = None) -> Dict[str, AbstractPredictResult]:
         all_combinations = self._generate_combinations(max_cmb_len)
 
         best_cmb, best_err = None, None
         for cmb in all_combinations:
             cur_err = self._calc_err_by_cmb(cmb)
+
+            self._result.add_cmb(combination=cmb, err=cur_err)
+
             if best_err is None or cur_err < best_err:
                 best_cmb, best_err = cmb, cur_err
-        return [self._predicts_results[c] for c in best_cmb]
+
+        self._result.set_result(combination=best_cmb, err=best_err)
+
+        return {c: self._predicts_results[c] for c in best_cmb}
 
     def _calc_err_by_cmb(self, combination: List[str]) -> float:
         errors = []
@@ -44,8 +95,8 @@ class BasicBagging:
             pass_indices(need_pass=True).get_loader()
 
         for dat in dp:
-            predict = self._calc_predict(dat['data_idx'], [self._predicts_results[c] for c in combination])
-            err = self._calc_error(predict, dat['target'])
+            predict = self._calc_predict(dat['data_idx'][0], [self._predicts_results[c] for c in combination])
+            err = self._calc_error(predict, self._pick_target(dat))
             errors.append(err)
 
         return np.mean(errors)
--- tests/pipeline/bagging.py
+++ tests/pipeline/bagging.py
@@ -1,6 +1,66 @@
+import os
+import shutil
 import unittest
+from typing import Any, Dict
+
+import numpy as np
+from piepline.data_producer import BasicDataset
+
+from pietoolbelt.pipeline.bagging.common import BasicBagging, BaggingResult
+from pietoolbelt.pipeline.predict.common import AbstractPredictResult
+
+
+class _PredictResultMock(AbstractPredictResult):
+    def __init__(self, predicts: Dict[str, float]):
+        self._predicts = predicts
+
+    def get_predict(self, index: str) -> Any:
+        return self._predicts[index]
+
+
+class _DatasetMock(BasicDataset):
+    def __init__(self, items: list):
+        super().__init__(items)
+
+    def _interpret_item(self, item) -> any:
+        return {'target': self._items[item]}
 
 
 class TestBasicBagging(unittest.TestCase):
+    RES_DIR = 'test_bagging_res'
+
+    def tearDown(self):
+        if os.path.exists(TestBasicBagging.RES_DIR):
+            shutil.rmtree(TestBasicBagging.RES_DIR, ignore_errors=True)
+
+    def test_init_with_incorrect_args(self):
+        with self.assertRaises(Exception):
+            BasicBagging(predicts_result={'r1': _PredictResultMock({'0': 1, '1': 0})}, calc_error=lambda x, y: x - y,
+                         dataset=_DatasetMock([1, 0]), reduce=np.mean, result=BaggingResult(path=TestBasicBagging.RES_DIR))
+
     def test_init(self):
-        raise NotImplementedError()
+        try:
+            BasicBagging(predicts_result={'r1': _PredictResultMock({'a': 1, 'b': 0}), 'r2': _PredictResultMock({'a': 2})},
+                         calc_error=lambda x, y: x - y, dataset=_DatasetMock([1, 0]), reduce=np.mean,
+                         result=BaggingResult(path=TestBasicBagging.RES_DIR))
+        except Exception as err:
+            self.fail("Fail instantiate BasicBagging. Error: [{}]".format(err))
+
+    def test_bagging(self):
+        bagging = BasicBagging(
+            predicts_result={'r1': _PredictResultMock({'0': 1, '1': 0}), 'r2': _PredictResultMock({'0': 2, '1': 0})},
+            calc_error=lambda x, y: x - y, dataset=_DatasetMock([1, 0]), reduce=np.mean,
+            result=BaggingResult(path=TestBasicBagging.RES_DIR))
+
+        res = bagging.run()
+        self.assertEqual(list(res.keys()), ['r1'])
+
+        shutil.rmtree(TestBasicBagging.RES_DIR, ignore_errors=True)
+
+        bagging = BasicBagging(
+            predicts_result={'r1': _PredictResultMock({'0': 1, '1': 2}), 'r2': _PredictResultMock({'0': 2, '1': 0})},
+            calc_error=lambda x, y: x - y, dataset=_DatasetMock([1, 0]), reduce=np.mean,
+            result=BaggingResult(path=TestBasicBagging.RES_DIR))
+
+        res = bagging.run()
+        self.assertEqual(list(res.keys()), ['r2'])
